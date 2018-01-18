4f7a85299ea29596fe192391a457e6d4d0c8d38a
Skylar Gordon <skylar.h.gordon@gmail.com>
2018-01-18 22:17:04 +0000

    Adding tasks 5 and 6, and their resulting executable files.

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - prints base10 numbers 0-9.
+* Return: 0 upon script completion.
+*/
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
