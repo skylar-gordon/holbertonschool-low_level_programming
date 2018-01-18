8c82d694db147f5add151cfc509b12567b94799a
Skylar Gordon <skylar.h.gordon@gmail.com>
2018-01-18 20:24:37 +0000

    Adding task 0 and resulting executable file.

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - will determine whether a random number is positive, negative, or 0.
+* Return: will return 0 upon completion of script.
+*/
+int main(void)
+{
+	int n;
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is 0\n", n);
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+holbertonschool-low_level_programming/0x01-variables_if_else_while
