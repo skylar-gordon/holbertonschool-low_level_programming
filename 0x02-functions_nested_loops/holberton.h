3516a5c059109eea5abfdb5d3dd63059980c5116
Skylar Gordon <skylar.h.gordon@gmail.com>
2018-01-22 20:47:45 +0000

    Added task 4 and resulting executable file. Edited holberton header file to accomodate task 4.

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <stdlib.h>
 #include <ctype.h>
 
 /**
@@ -47,4 +48,15 @@ int _islower(int c)
 		return (1);
 	else
 		return (0);
+}
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		if (c >= 97 && c <= 122)
+			return (1);
+		else
+			return (0);
 }
