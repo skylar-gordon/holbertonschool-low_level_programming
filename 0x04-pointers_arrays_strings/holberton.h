1d51c89bbb5079e7172172db94477ac1524843e4
Skylar Gordon <skylar.h.gordon@gmail.com>
2018-01-30 01:16:22 +0000

    Adding task 1-3.

--- 0x04-pointers_arrays_strings/holberton.h
+++ 0x04-pointers_arrays_strings/holberton.h
@@ -1,9 +1,55 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+#include <string.h>
+#include <stdio.h>
+
+/**
+* reset_to_98 - sets a provided integer to the value 98.
+* @n: the integer variable to change.
+*
+* Return: Void.
+*/
 
 void reset_to_98(int *n)
 {
 	*n = 98;
+}
+
+/**
+* swap_int - swaps the values of two integers.
+* @a: integer 1.
+* @b: integer 2.
+*
+* Return: Void.
+*/
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
+
+/**
+* _strlen - provides string length.
+* @s: The string in question.
+*
+* Return: length of string.
+*/
+
+int _strlen(char *s)
+{
+	char length = strlen(s);
+
+	return length;
+}
+
+void _puts(char *str)
+{
+	int i;
+	for (i = 0; i < _strlen(str); i++)
+		_putchar(str[i]);
 }
 
 #endif /* HOLBERTON_H */
