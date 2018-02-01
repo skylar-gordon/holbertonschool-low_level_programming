bdd332d1179d91900d9de51b341abdcd8b7ac4e7
Skylar Gordon <skylar.h.gordon@gmail.com>
2018-02-01 00:17:28 +0000

    Fixing task 8.

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -8,19 +8,12 @@
 */
 void print_array(int *a, int n)
 {
-	int i;
 	int j;
-	int arr[80];
 
-	for (i = 0; i < n; i++)
+	for (j = 0; j < n; j++)
 	{
-		arr[i] = a[i];
-	}
-
-	for (j = 0; j < i; j++)
-	{
-		printf("%d", arr[j]);
-		if (j != i - 1)
+		printf("%d", a[j]);
+		if (j != n - 1)
 			printf(", ");
 	}
 	printf("\n");
