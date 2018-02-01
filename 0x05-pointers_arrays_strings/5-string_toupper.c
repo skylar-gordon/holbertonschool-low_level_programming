2d916c13171e9eb3f4a3e6e357f7c3e09525b706
Skylar Gordon <skylar.h.gordon@gmail.com>
2018-02-01 23:42:38 +0000

    Added the betty description to task 5.

--- 0x05-pointers_arrays_strings/5-string_toupper.c
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,9 @@
 #include "holberton.h"
 #include "strlen.c"
 /**
-* 
+* string_toupper - makes the string uppercase.
+* @s: The string in question.
+* Return: uppercase string.
 */
 char *string_toupper(char *s)
 {
