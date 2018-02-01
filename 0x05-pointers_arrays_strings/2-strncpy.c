36a6e75e7d1f2e7648bd77c156e338181f43bfe3
Skylar Gordon <skylar.h.gordon@gmail.com>
2018-02-01 22:33:50 +0000

    Fixing task 2 in case of strlen(src) < n

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -13,7 +13,9 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < _strlen(src) && i < n; i++)
+	for (i = 0; i < n && i <= _strlen(src); i++)
 		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
