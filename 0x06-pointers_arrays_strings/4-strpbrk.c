a2b807f907469e22c99f2d9cb0d179bcdd2c8a48
Skylar Gordon <skylar.h.gordon@gmail.com>
2018-02-06 00:37:16 +0000

    Adding task 4.

---
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include "strlen.c"
+/**
+* _strpbrk - print matching characters
+* @s: The string
+* @accept: The characters that are matched against
+* Return: Pointer to first matching byte.
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < _strlen(s); i++)
+	{
+		for (j = 0; j < _strlen(accept); j++)
+			if (*(s + i) == *(accept + j))
+				break;
+		if (*(s + i) == *(accept + j))
+			break;
+	}
+	return ((s + i));
+}
