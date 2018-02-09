dbb3f507d5fd36b1986327575910603af0c98c08
Skylar Gordon <skylar.h.gordon@gmail.com>
2018-02-09 12:09:04 +0000

    Adding task 5.

---
+++ 0x09-argc_argv/100-change.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+* main - how many coins do I use to make change?
+* @argc: number of arguments.
+* @argv: arguments.
+* Return: 0 if giving 1 argument (in addition to running the exe), 1 upon error
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	int cents = atoi(argv[1]);
+	int change = 0;
+
+	if (cents >= 25)
+	{
+		change += cents / 25;
+		cents = cents % 25;
+	}
+
+	if (cents >= 10)
+	{
+		change += cents / 10;
+		cents = cents % 10;
+	}
+
+	if (cents >= 5)
+	{
+		change += cents / 5;
+		cents = cents % 5;
+	}
+
+	if (cents < 5)
+	{
+		change += cents;
+	}
+
+	printf("%d\n", change);
+	return (0);
+}
