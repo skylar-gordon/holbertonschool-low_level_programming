5040c4203bf66920ab6ecf30ba1bf8e44077f8ca
Skylar Gordon <skylar.h.gordon@gmail.com>
2018-02-12 21:33:55 +0000

    Ensuring ISO C90 compliance

--- 0x09-argc_argv/100-change.c
+++ 0x09-argc_argv/100-change.c
@@ -10,44 +10,40 @@
 
 int main(int argc, char *argv[])
 {
+	int cents;
+	int change;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	int cents = atoi(argv[1]);
-	int change = 0;
-
+	cents = atoi(argv[1]);
+	change = 0;
 	if (cents >= 25)
 	{
 		change += cents / 25;
 		cents = cents % 25;
 	}
-
 	if (cents >= 10)
 	{
 		change += cents / 10;
 		cents = cents % 10;
 	}
-
 	if (cents >= 5)
 	{
 		change += cents / 5;
 		cents = cents % 5;
 	}
-
 	if (cents >= 2)
 	{
 		change += cents / 2;
 		cents = cents % 2;
 	}
-
 	if (cents == 1)
 	{
 		change += 1;
 	}
-
 	printf("%d\n", change);
 	return (0);
 }
