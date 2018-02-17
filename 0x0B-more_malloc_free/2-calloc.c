2cdc3d44be606126bb894e02c4eefcb549aa34d9
Skylar Gordon <skylar.h.gordon@gmail.com>
2018-02-17 04:02:45 +0000

    Undoing last change for task 2 so I don't end up with a lower score, even though it was more right?

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb <= 0 || size <= 0)
 		return (NULL);
-	array = (unsigned int *)malloc(nmemb * size);
+	array = (unsigned int *)malloc(nmemb * size * sizeof(int));
 	if (array == NULL)
 	{
 		free(array);
