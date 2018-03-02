e5c5b4cbbfe24ade654215a903b0b5c2c13ed03b
Skylar Gordon <skylar.h.gordon@gmail.com>
2018-03-02 07:00:16 +0000

    Fixing task 2 for Betty, and completing task 3.

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- *
- *
- *
- *
+ * add_node - add a node to the beginning of the linked list.
+ * @head: Pointer to beginning of linked list.
+ * @str: Data to put in the new node.
+ * Return: Pointer to beginning of linked list upon success, NULL upon failure.
  */
 
 list_t *add_node(list_t **head, const char *str)
---
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * add_node_end - adds new node to end of linked list.
+ * @head: Pointer to first node in the linked list.
+ * @str: Data to go in the new node.
+ * Return: Pointer to first node upon success, NULL upon failure.
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	char *data = strdup(str);
+	list_t *new_node = (list_t *)malloc(sizeof(list_t));
+	list_t *last = *head;
+
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->str = data;
+	new_node->len = strlen(data);
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (*head);
+	}
+
+	while (last->next != NULL)
+		last = last->next;
+
+	last->next = new_node;
+	return (*head);
+}
